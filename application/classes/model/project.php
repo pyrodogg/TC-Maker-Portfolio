afa640df5c8d6aeeaabc1993597e1ce2c8a53e79
Skyler Kehren (MiniLappy) <pyrodogg@gmail.com>
2011-10-26 22:34:32 -0500

    add project model

--- application/classes/model/project.php
+++ application/classes/model/project.php
@@ -1,6 +1,9 @@
-<?php
+<?php defined('SYSPATH') or die('No direct script access.');
 
-class Model_Project extends Model{
+class Model_Project extends Automodeler_ORM{
 
+    protected $_table_name = 'project';
+
+    
 }
 ?>
